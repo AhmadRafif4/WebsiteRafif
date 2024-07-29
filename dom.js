39247e176fe5b85140dc9e34453bcdc1ada96c4e
AhmadRafif4 <ahmadrafif.ht@gmail.com>
2024-07-29 09:58:01 +0700

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,50 @@
+// 
+// let te = document.getElementById("div+1")
+// let te = document.getElementsByTagName("head")
+// let te = document.getElementsByClassName("div-2")
+
+// Query Selector
+// let te = document.querySelector("#div-1")
+// let te = document.querySelector(".div-2")
+// console.log(te)
+
+// set HTML Content
+// te.innerHTML = "<p>Hallo Dunia!</p>"
+
+// Change Attribute
+// let input= document.getElementById("input")
+// console.log(input)
+
+// input.setAttribute("type", "checkbox")
+// let te = document.getElementById("input")
+// input.style.bordercolor = "red"
+// input.style.color = "coral"
+
+// const pElement = document.createElement("p")
+// const divEl = document.getElementById("div-1")
+
+// divEl.appendChild(pElement)
+// pElement.innerHTML = "Halo Dunia p"
+
+// divEl.removeChild(pElement)
+
+// const hello = document.getElementById("hello")
+
+// hello.addEventListener("mouseenter", function () {
+//     // console.log("Mouse enter hallo dunia text")
+//     hello.style.border = "3px solid green"
+// })
+
+// const btn = document.getElementById("btn")
+// btn.addEventListener("click", function(){
+//     const duaar = document.getElementById("boom")
+//     duaar.style.display = "block"
+// })
+
+function showduaar() {
+    const duaar = document.getElementById("duaar")
+    duaar.style.display = "block"
+    
+}import function kamu(params) {
+    
+}
---
+++ main.js
@@ -0,0 +1,38 @@
+let test = "bang"
+
+console.log(test);
+
+test = test + 43;
+
+console.log(test);
+
+test = test + 8;
+
+console.log(test);
+
+test = test + 222;
+
+console.log(test);
+
+test = test --;
+
+console.log(test);
+
+// numerik adalah angka
+
+let ter = 33;
+ter = ter < 
+console.log(ter);
+
+function ok (num1, num2, num3, num4, num5, num6) {
+    console.log(num1 - num3); 
+    console.log(num2 * num4);
+    console.log(num5 / num6);
+}
+
+ok (15, 10, 2, 2, 30, 5)
+
+function qw (name1, name2, name3, name4) {
+    console.log("bang" + name1 + name2 + name3 + " anak kecil," + " aku" + name2 + name4);
+}
+qw (" aku ", "sudah", " bukan", " dewasa")
